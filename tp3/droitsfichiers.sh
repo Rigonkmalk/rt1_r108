bf2664ef97cb17dc25d4bb17984fbc15eb7a709e
Paul Azema <paulazema@MacBook-Pro-3.local>
2022-10-17 16:27:13 +0200

    chore(tp3/droits): lint file

--- tp3/droitsfichiers.sh
+++ tp3/droitsfichiers.sh
@@ -2,7 +2,7 @@
 regex="^([+-][rR]|[+-][wW])$"
 
 if [ $# -le 0 ]; then
-    echo "error: no args"
+    echo "Error: no args"
 elif [[ $1 =~ $regex ]]; then
     for i in *"$2"; do 
         chmod g$1 $i
