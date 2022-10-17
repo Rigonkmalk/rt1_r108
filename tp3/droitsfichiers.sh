d6c7f582fef5efcc4282917aee8755feaee54712
Paul Azema <paulazema@MacBook-Pro-3.local>
2022-10-17 16:09:50 +0200

    feat(tp3/droits): replace fi with elif

--- tp3/droitsfichiers.sh
+++ tp3/droitsfichiers.sh
@@ -3,8 +3,7 @@ regex="^([+-][rR]|[+-][wW])$"
 
 if [ $# -le 0 ]; then
     echo "error: no args"
-fi
-if [[ $1 =~ $regex ]]; then
+elif [[ $1 =~ $regex ]]; then
     for i in *"$2"; do 
         chmod g$1 $i
     done
